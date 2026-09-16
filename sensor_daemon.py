269540f1d7f66d2b508acaa83241f1b58e0476a8
Chrysanthemum Gribble Gilbert <sea.gilbert002@gmail.com>
2026-09-16 16:11:33 -0400

    Add environment variables with defaults

--- sensor_daemon.py
+++ sensor_daemon.py
@@ -16,4 +16,8 @@ import sensor_hal
 
 # Environment Variable Configuration
 API_URL = os.getenv("TELEMETRY_API_URL", "http://localhost:3000/api/telemetry")
+SHELF_ID = os.getenv("SHELF_ID", "LOFT_SHELF_2")
+SAMPLING_INTERVAL_SEC = int(os.getenv(" SAMPLING_INTERVAL", "30"))
+INITIAL_BACKOFF_SEC = 1
+MAX_BACKOFF_SEC = 60
 
